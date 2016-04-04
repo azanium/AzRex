b13c68627d5b8262c78e51e98df9afffed87cffc
Suhendra Ahmad <syuaibi@gmail.com>
2016-04-04 19:12:45 +0700

    Initial commit

---
+++ AzRex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AzRex
+//
+//  Created by Suhendra Ahmad on 4/4/16.
+//  Copyright © 2016 Suhendra Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+inhibit_all_warnings!
+
+target 'AzRex' do
+
+pod 'IFVideoPicker', '~> 1.0.2'
+pod 'SDWebImage'
+
+
+#post_install do |installer|
+# installer.project.targets.each do |target|
+#    target.build_configurations.each do |config|
+#      config.build_settings['GCC_WARN_INHIBIT_ALL_WARNINGS'] = 'YES'
+#    end
+#  end
+#end
+
+end
